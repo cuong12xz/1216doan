8a904c329659d8546ec463fd6b9264daa0f0cef8
cuong <ngoanhcuong21@gmail.com>
2021-12-16 16:23:31 +0700

    khoi tao project

---
+++ dangnhap.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Liên Hệ</title>
+
+    <?php include 'layout/head.php'; ?>
+    <style>
+        .form-horizontal{
+            margin-right:auto;
+            margin-left:20%;
+            
+        }
+        
+    </style>
+</head>
+
+<body class="goto-here">
+    <!--Nav-->
+    <?php include 'layout/menu.php' ?>
+    <!-- END nav -->
+
+
+
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 clearpadding" style="margin-top: 15px;">
+        <ol class="breadcrumb">
+            <li><a href="#"><span class="glyphicon glyphicon-home" aria-hidden="true"></span></a></li>
+            <li class="active">Đăng nhập</li>
+        </ol>
+        <div class="panel panel-info ">
+            <div class="panel-body">
+                <form class="form-horizontal" method="post" action="#">
+                    <div class="form-group">
+                        <h3 style="color: red"></h3>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputEmail3" class=" col-sm-offset-2 col-sm-2 control-label">Email</label>
+                        <div class="col-sm-4">
+                            <input type="email" class="form-control" id="inputEmail3" placeholder="" name="email" value="">
+                        </div>
+                        <div class="col-sm-3">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-offset-2 col-sm-2 control-label">Mật khẩu</label>
+                        <div class="col-sm-4">
+                            <input type="password" class="form-control" id="inputEmail3" placeholder="" name="password">
+                        </div>
+                        <div class="col-sm-3">
+                        </div>
+                    </div>
+                    <div class="form-group dndk">
+                        <div class="col-sm-offset-4 ">
+                            <button type="submit" class="btn btn-success">Đăng nhập</button>
+                        
+                        
+                            <span style="margin-left: 10%;"><a href="dangky.php">Đăng kí</a></span>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- footer -->
+    <?php include 'layout/foot.php'; ?>
+    <!-- footer -->
+
+
+    <!-- loader -->
+    <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
+            <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+            <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00" />
+        </svg></div>
+
+
+    <!--script-->
+    <?php include 'layout/script.php'; ?>
+    <!--end script-->
+
+</body>
+
+</html>
---
+++ layout/menu.php
@@ -0,0 +1,31 @@
+<!--nav-->
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+        <div class="container">
+          <a class="navbar-brand" href="index.php">Winkel</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="oi oi-menu"></span> Menu
+          </button>
+
+          <div class="collapse navbar-collapse" id="ftco-nav">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active"><a href="index.php" class="nav-link">Trang Chủ</a></li>
+              <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Cửa Hàng</a>
+              <div class="dropdown-menu" aria-labelledby="dropdown04">
+                <a class="dropdown-item" href="shop.php">Cửa Hàng</a>
+                <a class="dropdown-item" href="product-single.php">Sản Phẩm Đơn</a>
+                <a class="dropdown-item" href="cart.php">Giỏ</a>
+                <a class="dropdown-item" href="checkout.php">Thanh Toán</a>
+              </div>
+            </li>
+              <li class="nav-item"><a href="about.php" class="nav-link">Về Chúng Tôi</a></li>
+              <li class="nav-item"><a href="blog.php" class="nav-link">Blog</a></li>
+              <li class="nav-item"><a href="contact.php" class="nav-link">Liên Hệ</a></li>
+              <li class="nav-item cta cta-colored"><a href="cart.php" class="nav-link"><span class="icon-shopping_cart"></span>[0]</a></li>
+              <li class="nav-item"><a href="dangnhap.php" class="nav-link">Đăng nhập</a></li>
+
+            </ul>
+          </div>
+        </div>
+      </nav>
+    <!-- END nav -->
